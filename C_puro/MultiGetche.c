1a618075163ffd28bd7a9e48af242ab545b7b449
LM1403 <guilherme.dsmoreira@gmail.com>
2021-04-20 19:28:02 -0300

    C puro

---
+++ C_puro/MultiGetche.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+char* multiploGetcheNumerico(int quantDigitos)
+{
+  char* string;
+  int i = 0;
+  while(i < quantDigitos)
+  {
+    char caractere = getch();
+    if(caractere >= '0' && caractere <= '9')
+    {
+      printf("%c", caractere);
+      *(string + i) = caractere;
+      i++;
+    }
+    else if(caractere == 8)
+    {
+      printf("%c%c%c%c", 0, 8, 0, 8);
+      if(i != 0)
+      {
+          i--;
+      }
+    }
+  }
+  return string;
+}
+
+int main() 
+{
+    char* string = multiploGetcheNumerico(2); 
+
+    printf("\n");
+    int i = 0;
+    for(; i < 2; i++){
+        printf("%c", *(string + i));
+    }
+    return 0;
+}
