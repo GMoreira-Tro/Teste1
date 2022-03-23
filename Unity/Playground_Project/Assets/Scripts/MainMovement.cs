3b48a40c5649590a3e98cb28e68e373a22f0aa62
I559776 <guilherme.dsmoreira@gmail.com>
2022-03-23 14:46:37 -0300

    a

---
+++ Unity/Playground_Project/Assets/Scripts/MainMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MainMovement : MonoBehaviour
+{
+    public float movingSpeed = 1f;
+    public float rotationSpeed = 1f;
+
+    private Mainmovement mainMovement;
+
+    private void Start()
+    {
+        mainMovement = new Mainmovement();
+        mainMovement.Main.Enable();
+    }
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+    public void Move()
+    {
+        float buttonValue = mainMovement.Main.Move.ReadValue<float>();
+        transform.Translate(buttonValue * movingSpeed * Time.deltaTime * transform.forward);
+    }
+    public void Rotate()
+    {
+        float buttonValue = mainMovement.Main.Rotate.ReadValue<float>();
+        transform.Rotate(buttonValue * rotationSpeed * Time.deltaTime * transform.up);
+    }
+}
